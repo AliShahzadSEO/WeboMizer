687c5827c13a4e4a2c6bf55789180f3a60098846
Habib <habibmakik1942@gmail.com>
2024-11-05 18:15:38 +0500

    updated

--- main.py
+++ main.py
@@ -2,27 +2,38 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from site_analyzer import analyze_competitor_site, generate_recommendations
 
 app = Flask(__name__)
-app.secret_key = '94751c46ad63108d32348baa2ae4d9e2'
+app.secret_key = '94751c46ad63108d32348baa2ae4d9e2'  # Required for session management
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/competitors', methods=['POST'])
-def competitors():
+@app.route('/project_details', methods=['POST'])
+def project_details():
     business_info = {
         'business_name': request.form['business_name'],
         'business_url': request.form['business_url'],
         'business_description': request.form['business_description'],
         'keywords': request.form['keywords'],
+        'industry': request.form['industry'],
+        'solutions': request.form['solutions'],
+        'skills': request.form['skills'],
     }
     session['business_info'] = business_info
     return render_template('competitors.html')
 
-@app.route('/site_architecture', methods=['POST'])
+@app.route('/competitors', methods=['GET', 'POST'])
+def competitors():
+    if request.method == 'POST':
+        competitor_urls = request.form.get('competitor_urls', '').split(',')
+        session['competitor_urls'] = competitor_urls
+        return redirect(url_for('site_architecture'))
+    return render_template('competitors.html')
+
+@app.route('/site_architecture')
 def site_architecture():
     business_info = session.get('business_info', {})
-    competitor_urls = request.form['competitor_urls'].split(',')
+    competitor_urls = session.get('competitor_urls', [])
     
     competitor_analysis = []
     for url in competitor_urls:
@@ -37,21 +48,5 @@ def site_architecture():
                            recommended_structure=recommended_structure,
                            recommended_navbar=recommended_navbar)
 
-@app.route('/competitors', methods=['POST'])
-def competitors():
-    business_info = {
-        'business_name': request.form['business_name'],
-        'business_url': request.form['business_url'],
-        'business_description': request.form['business_description'],
-        'keywords': request.form['keywords'],
-        'industry': request.form['industry'],
-        'solutions': request.form['solutions'],
-        'skills': request.form['skills'],
-    }
-    session['business_info'] = business_info
-    return render_template('competitors.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
